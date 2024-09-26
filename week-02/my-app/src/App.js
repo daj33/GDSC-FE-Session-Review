7a7d24b164be9f994bd142cec8aec5bfaa8d7c64
ttf416 <thefirstfruit416@gmail.com>
2024-09-26 21:03:37 +0900

    초기 설정

---
+++ week-02/my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const subject = "React";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, {subject}!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
